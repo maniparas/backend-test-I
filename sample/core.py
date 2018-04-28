6d86cf38f4f6c74e9e4a4925a58727bf7b757675
Paras Mani <maniparas15@gmail.com>
2018-04-28 20:19:41 +0530

    final commit for the project

--- sample/core.py
+++ sample/core.py
@@ -1,12 +1,83 @@
 import tweepy
+import re
 from docs.conf import consumer_key, consumer_secret, access_token_secret, access_token
 from time import sleep
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+from sample.spreadsheet import GSpreadSheet
 
-# For Secured Connection
-auth.secure = True
-api = tweepy.API(auth)
 
-print(str(api.get_user(screen_name='@maniparas')))
+def init_configuration():
+    """
+    This method is used for initial configuration to set up the secure authentication
+    :return: AuthHandler
+    """
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    # For Secured Connection
+    auth.secure = True
+
+    return auth
+
+
+class MyCustomBot(object):
+    def __init__(self):
+        auth = init_configuration()
+        self.api = tweepy.API(auth)
+
+    def find_tweets(self, q='#CongManifestoScam'):
+        for tweet in tweepy.Cursor(self.api.search, q=q, lang='en').items(10):
+            try:
+                username = tweet.user.screen_name
+                followers_count = tweet.user.followers_count
+                print("Found tweet by: @" + username, followers_count)
+            except tweepy.TweepError as e:
+                print(e.reason)
+                sleep(10)
+                continue
+            except StopIteration:
+                break
+
+
+class MyCustomBotListener(tweepy.StreamListener):
+    def on_error(self, status_code):
+        """
+        This method would be used to handle any kind of error,
+        we can use this method to disconnect the listener if already active
+        :param status_code:
+        :return:
+        """
+
+    def on_status(self, status):
+        """
+        This method would be used whenever there is a new status from the stream
+        we can use this method to perform our action
+        :param status:
+        :return:
+        """
+        username = status.user.screen_name
+        count_followers = status.user.followers_count
+
+        data_to_write = [username, count_followers]
+        spreadsheet = GSpreadSheet()
+
+        file_name = 'TestHashtagBot'
+        spreadsheet.write(file=file_name, data_to_write=data_to_write)
+
+
+if __name__ == "__main__":
+    text = input(" Enter comma separated hash tags, with hash tag symbol :- ")
+    # Find all tags
+    tags = re.findall(r"[\w']+", text)
+    hash_tags = [tag for tag in tags]
+    if hash_tags:
+        my_auth = init_configuration()
+        stream_listener = MyCustomBotListener()
+        stream = tweepy.Stream(my_auth, stream_listener)
+        print(f'[-] Following are the hash tags : {hash_tags}')
+        print('-' * 80)
+        stream.filter(track=hash_tags)
+
+
+# bot = MyCustomBot()
+# sheet = GSpreadSheet()
+# bot.find_tweets()
---
+++ sample/spreadsheet.py
@@ -0,0 +1,84 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+class GSpreadSheet(object):
+    def __init__(self):
+        try:
+            scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+            credentials = ServiceAccountCredentials \
+                .from_json_keyfile_name('../Test Hashtag Bot-85bcf54c6a5e.json', scope)
+            self.gc = gspread.authorize(credentials)
+        except FileNotFoundError as e:
+            print(e.reason)
+
+    def open(self, file='TestHashtagBot'):
+        """
+        This method would be used to open a provided spreadsheet,
+        If spreadsheet isn't provided in the argument, default spreadsheet would be opened
+        :param file:
+        :return:
+        """
+        try:
+            worksheet = self.gc.open(file)
+            return worksheet
+        except gspread.SpreadsheetNotFound as e:
+            print(e.reason)
+
+    def read(self, worksheet):
+        return worksheet.get_all_records()
+
+    def delete(self, worksheet):
+        """
+        This method can be used to delete the worksheet,
+        since I am not using it so leaving it blank for future implementation
+        :param worksheet:
+        :return:
+        """
+
+    def is_empty(self, data):
+        """
+        This method checks whether the spreadsheet is empty or not
+        :param data:
+        :return:
+        """
+        count = 0
+        for value in data:
+            if value and value != ['']:
+                count += 1
+        return count == 0
+
+    def write(self, worksheet=None, file=None, data_to_write=None):
+        """
+        This method would write data in spreadsheet,
+        If spreadsheet is_empty only column headers would be written, else data would be appended to sheet
+        :param worksheet:
+        :param file:
+        :param data_to_write:
+        :return:
+        """
+        if not worksheet:
+            try:
+                worksheet = self.open(file)
+            except gspread.SpreadsheetNotFound as e:
+                print(e.reason)
+
+        wks = worksheet.sheet1
+
+        # Try to read data, if no data is present through message
+        data = None
+        try:
+            data = self.read(wks)
+        except IndexError:
+            print("Spreadsheet is empty")
+            # If the data is empty add only headings
+            wks.append_row(['Profile Name', 'Followers Count'])
+
+        # # If the data is empty add only headings
+        # if self.is_empty(data):
+        #     wks.update_cell('A1', 'Profile Name')
+        #     wks.update_cell('B1', 'Followers Count')
+
+        # If data isn't empty append records
+        wks.append_row(data_to_write)
+        return wks
