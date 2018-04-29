ad7239be1dd0a233b5e42543503d2d2d39c7aab9
Paras Mani <maniparas15@gmail.com>
2018-04-29 19:43:23 +0530

    removed twitter app credentials from conf file

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,5 @@
 # App Credentials
-consumer_key = 'fkYpJ1w1MdTy0eVDji8qDcNBj'
-consumer_secret = 'GC1KclV6EvOpxlCNYQEQJZJ0hlei4QZa3Hxkin72zoVx3PtAYL'
-access_token = '2732069042-CGakeHjnXqzMFfckcS8StfnZXIkBGXsSt1QyLLC'
-access_token_secret = 'I7ud1LIjFz31fB9dD8KHcJus7hCumLic2qnp6XTcCMekL'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
